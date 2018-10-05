1b43c6eb104d39ee3833b6501b15e2b3ab6aeb8a
Janik Rabe <mail@janik-rabe.de>
2018-10-05 09:58:20 +0200

    Add function to find customer www-root and save the keys there

--- plesk-dkim-recreate.sh
+++ plesk-dkim-recreate.sh
@@ -7,16 +7,19 @@ tmp_prefix=dkim_tmpfile_
 ##temp file ending
 tmp_fe=.txt
 
-##path for dkim file
-dkim_rec_path=/tmp/dkim_keys
+##path in customer www-root for dkim file
+dkim_rec_path=/dkim_keys
 ##dkim filename prefix
 dkim_rec_prefix=dkim_key_
 ##dkim file ending
 dkim_rec_fe=.txt
 
+##find customer www-root
+customer_root=$(/usr/local/psa/bin/domain --info ${NEW_DOMAIN_NAME} | grep "WWW-Root" | cut -d/ -f2-5)
+
 ## create directories if they do not exist yet
 mkdir -p $tmp_path
-mkdir -p $dkim_rec_path
+mkdir -p /$customer_root$dkim_rec_path
 
 ## enable DKIM on the domain
 /usr/local/psa/bin/domain_pref --update ${NEW_DOMAIN_NAME} -sign_outgoing_mail true
@@ -32,4 +35,4 @@ dkim_key=$(tr -d '\n' < $tmp_path/$tmp_prefix${NEW_DOMAIN_NAME}$tmp_fe)
 rm $tmp_path/$tmp_prefix${NEW_DOMAIN_NAME}$tmp_fe
 
 ## generate domainkey record and store to text file
-echo "default._domainkey.${NEW_DOMAIN_NAME} IN TXT \"v=DKIM1; k=rsa; p=$dkim_key" > $dkim_rec_path/$dkim_rec_prefix${NEW_DOMAIN_NAME}$dkim_rec_fe
+echo "default._domainkey.${NEW_DOMAIN_NAME} IN TXT \"v=DKIM1; k=rsa; p=$dkim_key" > /$customer_root$dkim_rec_path/$dkim_rec_prefix${NEW_DOMAIN_NAME}$dkim_rec_fe
